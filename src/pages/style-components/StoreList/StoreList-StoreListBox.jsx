a0a160bd4ee4cfbd37fe605fc0033a7e63b5306d
CroppedEyelid <lee940706@gmail.com>
2023-11-15 09:38:32 +0900

    메인페이지 스타일컴포넌트 추가

--- src/pages/style-components/StoreList/StoreList-StoreListBox.jsx
+++ src/pages/style-components/StoreList/StoreList-StoreListBox.jsx
@@ -1,5 +1,62 @@
 import styled from "styled-components";
 
+export const CarouselBox = styled.div`
+  display: flex;
+  width: 255px;
+  height: 200px;
+  background-color: #fff
+  margin-left: 33px;
+  border: 1px solid #666;
+  display: flex;
+  flex-direction: row;
+  background-size: contain;
+  overflow: hidden;
+`;
+
+export const PaginationBox = styled.div`
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    position: relative;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  ul.pagination li {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #e2e2e2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+  }
+  ul.pagination li:first-child {
+    border-radius: 5px 0 0 5px;
+  }
+  ul.pagination li:last-child {
+    border-radius: 0 5px 5px 0;
+  }
+  ul.pagination li a {
+    text-decoration: none;
+    color: #337ab7;
+    font-size: 1rem;
+  }
+  ul.pagination li.active a {
+    color: white;
+  }
+  ul.pagination li.active {
+    background-color: #337ab7;
+  }
+  ul.pagination li a:hover,
+  ul.pagination li a.active {
+    color: blue;
+  }
+`;
+
 export const StoreListBox01 = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
@@ -20,16 +77,6 @@ export const StoreItem = styled.div`
   position: relative;
   box-shadow: 0px 3px 14px -5px #555555;
 `;
-
-// export const StoreIMGBOX = styled.img`
-//   width: 255px;
-//   height: 200px;
-//   /* background-color: #666; */
-//   margin-left: 33px;
-//   border: 1px solid #666;
-//   display: flex;
-//   flex-direction: row;
-// `;
 
 export const HeartIcon01 = styled.img`
   height: 30px;
@@ -52,6 +99,16 @@ export const StorListInfo01 = styled.div`
 `;
 
 export const StoreType01 = styled.div`
+  width: 800px;
+  height: 24px;
+  display: flex;
+  font-size: 20px;
+  position: relative;
+  left: 8px;
+  color: #111;
+`;
+
+export const StoreTag = styled.div`
   width: 800px;
   height: 24px;
   display: flex;
